837d4b681cb245210d661228880d2f870666a06d
wbucko <bucko.wojciech@gmail.com>
2015-10-31 11:36:40 +0000

    Added form partial and edit action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -15,6 +15,10 @@ class ProfilesController < ApplicationController
            render action: :new
        end
     end
+    def edit
+        @user = User.find(params[:user_id])
+        @profile = @user.profile
+    end
     
     
     
